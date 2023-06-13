be4c21f79828c1370be8e570da7eb3db03384e0a
Jules Aubert <aubertjules@gmail.com>
2023-06-13 02:08:08 +0200

    Begin exercise, testsuite written.

---
+++ rust/exercises/binary_search/README.md
@@ -0,0 +1,33 @@
+# Docs
+
+**This is just an exercise**
+
+You should use
+```rust
+vector.get(); // with a match
+```
+to test presence of an item.
+
+## Algorithm
+The array must be sorted.
+
+Binary search get the index of a searched element in a logarithmic complexity
+using dichotomy.
+
+If the min and max indices of the sub-vector are equal or reversed (max < min),
+the search is negative (we did not find the element).
+
+Otherwise, the middle element of the vector is chosen as a pivot.
+* If searched element = pivot, it returns its position;
+* If the searched element is greater than the pivot, it loops back the search on
+  the sub-vector, starting at the (pivot + 1) position.
+* If the searched element is less than the pivot, it loops back the search on
+  the sub-vector, ending at the (pivot - 1) position.
+
+You can generate the doc with:
+```shell
+rustdoc README.md
+```
+
+## Testsuite
+Execute *cargo test* to execute the testsuite.
---
+++ rust/exercises/binary_search/src/lib.rs
@@ -0,0 +1,69 @@
+//! Of course the best way to search for an item is to use vec.get() and match the result. This is
+//! just an exercise.
+
+pub fn binary_search(vec: Vec<i32>, elt: i32) -> i32 {
+    println!("{:?}", vec);
+    println!("{elt}");
+    -2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn absent() {
+        let v = vec![-19, -17, -13, -11, -7, -5, -3, -2, 2, 3, 5, 7, 11, 13, 17, 19];
+        let expected = -1;
+        let result = binary_search(v, 0);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn first() {
+        let v = vec![-19, -17, -13, -11, -7, -5, -3, -2, 2, 3, 5, 7, 11, 13, 17, 19];
+        let expected = 0;
+        let result = binary_search(v, -19);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn middle() {
+        let v = vec![-19, -17, -13, -11, -7, -5, -3, -2, 0, 2, 3, 5, 7, 11, 13, 17, 19];
+        let expected = 8;
+        let result = binary_search(v, 0);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn last() {
+        let v = vec![-19, -17, -13, -11, -7, -5, -3, -2, 2, 3, 5, 7, 11, 13, 17, 19];
+        let expected = 15;
+        let result = binary_search(v, 19);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn empty() {
+        let v = Vec::new(); 
+        let expected = -1;
+        let result = binary_search(v, 19);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn one_element_absent() {
+        let v = vec![-19];
+        let expected = -1;
+        let result = binary_search(v, 19);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn one_element_present() {
+        let v = vec![-19];
+        let expected = 0;
+        let result = binary_search(v, -19);
+        assert_eq!(result, expected);
+    }
+}
