22ea898fe500ef7eca6f98b449d74ccadbe24c98
Jules Aubert <aubertjules@gmail.com>
2023-06-13 04:24:00 +0200

    Begin exercise, testsuite written.

---
+++ rust/exercises/bubble_sort/README.md
@@ -0,0 +1,26 @@
+# Docs
+
+**This is just an exercise**
+
+You should use the real
+```rust
+vector.sort();
+```
+to sort a vector.
+
+## Algorithm
+
+One of the most basic sorting algorithm.
+
+It compares each adjacent items and swaps them if not in the right order.
+
+## Documentation
+
+You can generate the doc with:
+```shell
+rustdoc README.md
+```
+
+## Testsuite
+
+Execute *cargo test* to execute the testsuite.
---
+++ rust/exercises/bubble_sort/src/lib.rs
@@ -0,0 +1,67 @@
+pub fn bubble_sort(vec: &mut Vec<i32>) {
+    vec[0] = i32::MAX;
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn vec_is_sorted(vec: Vec<i32>) -> bool {
+        for i in 1..vec.len() {
+            if vec[i - 1] > vec[i] {
+                return false;
+            }
+        }
+        true
+    }
+
+    #[test]
+    fn simple() {
+        let mut vec = vec![42, 11, 73, 66, 0, -8, 100, 99];
+        bubble_sort(&mut vec);
+        assert!(vec_is_sorted(vec));
+    }
+
+    #[test]
+    fn multiple() {
+        let mut vec = vec![42, 42, 42, 11, 11, 73, 73, 73, 66, 66, 66, 66, 66, 0, 0, -8, -8, 100, 100, 99, 99, 99, 99, 99, 99];
+        bubble_sort(&mut vec);
+        assert!(vec_is_sorted(vec));
+    }
+
+    #[test]
+    fn reversed() {
+        let mut vec = vec![100, 99, 73, 66, 42, 11, 0, -8];
+        bubble_sort(&mut vec);
+        assert!(vec_is_sorted(vec));
+    }
+
+    #[test]
+    fn one() {
+        let mut vec = vec![42];
+        bubble_sort(&mut vec);
+        assert!(vec_is_sorted(vec));
+    }
+
+    #[test]
+    fn two_sorted() {
+        let mut vec = vec![11, 42];
+        bubble_sort(&mut vec);
+        assert!(vec_is_sorted(vec));
+    }
+
+    #[test]
+    fn two() {
+        let mut vec = vec![42, 11];
+        bubble_sort(&mut vec);
+        assert!(vec_is_sorted(vec));
+    }
+
+    #[test]
+    fn empty() {
+        let mut vec = Vec::new();
+        bubble_sort(&mut vec);
+        assert!(vec_is_sorted(vec));
+    }
+}
